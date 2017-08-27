6229f125a63de2c199f33d9ba68e6d9314a59ebd
Teo Fleming <mokolodi1@gmail.com>
2017-08-27 16:23:51 +0200

    improved sum stub

--- server.js
+++ server.js
@@ -24,8 +24,13 @@ app.route('/v1/calculate_sum').get(function(req, res) {
   console.log("Someone's requesting information!");
   console.log("req.query:", req.query);
 
-  var first = Integer.parseInt(req.query.first)
-  var second = Integer.parseInt(req.query.second)
+  var first = parseInt(req.query.first)
+  var second = parseInt(req.query.second)
+
+  if (first === NaN || second === NaN) {
+    res.status(400); // 400 = bad request
+    res.send("You didn't put the right parameters!");
+  }
 
   res.json({
     sum: first + second
